8f1af164eea2f37afc0dc0c3c96bdcca24fd1f30
TS-CB <TS-CB@cbmac.local>
2016-05-12 14:52:39 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by TS-CBin on 16/5/12.
+//  Copyright © 2016年 TS-CBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by TS-CBin on 16/5/12.
+//  Copyright © 2016年 TS-CBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
